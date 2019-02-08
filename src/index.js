a1fa3ef6df80993e2fb90f9ce50771b550704abf
p-racoon <prasham.ashesh>
2019-02-08 10:34:25 +0530

    interactive Button, raises an Alert

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
+        <button className="square" onClick={()=>(alert("button clicked"))}>
             {/* Here the value is printed  */}
           {this.props.value}
         </button>
